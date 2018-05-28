648d6d13676ec35a6347ae3ce9edb8e832a2c1de
Paul <a3698147852@gmail.com>
2018-05-28 13:50:56 +1000

    change color from green to dark green, change drawsquare to draw plus, change shape

--- BookApi/OLR_Test.py
+++ BookApi/OLR_Test.py
@@ -1,12 +1,18 @@
 import turtle as t
-t.color ('green')
+#t.color ('darkkhaki')
+t.colormode(255)
+t.pencolor(21, 95, 30)
 t.shape ('turtle')
 t.speed (3)
 
-def drawsquare():
+def drawplus():
     for x in range(4):
+        t.fd(90)
+        t.left(90)
+        t.fd(90)
+        t.right(90)
         t.fd(90)
         t.left(90)
 
 
-drawsquare()
+drawplus()
